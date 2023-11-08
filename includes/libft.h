36c3005f08baa0589b598e9bd052c0509e086f18
cefuente <cfuen55010@gmail.com>
2023-11-08 12:07:33 +0100

    log

--- includes/libft.h
+++ includes/libft.h
@@ -6,20 +6,31 @@
 /*   By: cefuente <cefuente@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/07 10:12:29 by cefuente          #+#    #+#             */
-/*   Updated: 2023/11/08 09:25:25 by cefuente         ###   ########.fr       */
+/*   Updated: 2023/11/08 11:35:40 by cefuente         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef LIBFT_H
 # define LIBFT_H
 
-int ft_isalpha(int c);
-int ft_isdigit(int c);
-int ft_isalnum(int c);
-
 #include <stdio.h>
 #include <unistd.h>
 #include <stdlib.h>
 #include <ctype.h>
+
+int ft_isalpha(int c);
+int ft_isdigit(int c);
+int ft_isalnum(int c);
+int ft_isascii(int c);
+int ft_isprint(int c);
+int ft_strlen(char *str);
+size_t	ft_strlcpy(char *dest, char *src, size_t size);
+size_t	ft_strlcat(char *dest, char *src, size_t size);
+int ft_toupper(int c);
+int ft_tolower(int c);
+int ft_strchr(const char *s, int c);
+int ft_strrchr(const char *s, int c);
+int	ft_strncmp(char *s1, char *s2, size_t n);
+char	*ft_strnstr(char *str, char *to_find, size_t len);
 
 #endif
