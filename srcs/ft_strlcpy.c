791fb58705f0ee1db09944e2ab9354625c76b5e8
cefuente <cefuente@student.42.fr>
2023-11-09 10:01:48 +0100

    log

--- srcs/ft_strlcpy.c
+++ srcs/ft_strlcpy.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strlcpy.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: cefuente <cefuente@student.42.fr>          +#+  +:+       +#+        */
+/*   By: cesar <cesar@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/08 09:42:08 by cefuente          #+#    #+#             */
-/*   Updated: 2023/11/09 09:44:53 by cefuente         ###   ########.fr       */
+/*   Updated: 2023/11/09 10:00:22 by cesar            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,18 +31,17 @@ size_t	ft_strlcpy(char *dest, char *src, size_t size)
 
 #include "libft.h"
 #include <ctype.h>
-#include <string.h>
 #include <stdio.h>
 #include <bsd/string.h>
 
-int	main(int argc, char **argv)
+int	main(void)
 {
-	if (argc < 3)
-		return (-1);
+/* 	if (argc < 3)
+		return (-1); */
 	char *dest1 = " ";
 	char *dest2 = " ";
-	printf("OG is %zu\n", strlcpy(dest1, argv[1], atoi(argv[2])));
-	printf("mine is %zu\n", ft_strlcpy(dest2, argv[1], atoi(argv[2])));
+	printf("OG is %zu\n", strlcpy(dest1, "abeilles", 4));
+	printf("mine is %zu\n", ft_strlcpy(dest2, "abeilles", 4));
 	printf("OG gives %s\nMine gives %s\n", dest1, dest2);
 	return (0);
 }
