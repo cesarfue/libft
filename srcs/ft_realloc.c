325ea00478e9cc3ad9cd115f5fb7baf99ad89aa1
cefuente <cfuen55010@gmail.com>
2024-02-02 15:54:12 +0100

    removed malloc_er

--- srcs/ft_realloc.c
+++ srcs/ft_realloc.c
@@ -6,7 +6,7 @@
 /*   By: cefuente <cefuente@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/26 13:24:37 by cesar             #+#    #+#             */
-/*   Updated: 2024/02/02 15:16:48 by cefuente         ###   ########.fr       */
+/*   Updated: 2024/02/02 15:48:32 by cefuente         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,9 @@ void	*ft_realloc(void *ptr, size_t old_size, size_t new_size)
 {
 	void	*ret;
 
-	ret = malloc_er(new_size);
+	ret = malloc(new_size);
+	if (!ret)
+		return (NULL);
 	if (!ptr)
 		return (ret);
 	ft_memmove(ret, ptr, old_size);
