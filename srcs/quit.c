71b209b6c2ab0c17df73dbbaa00afc6284816553
cesar <cesar.fuentes@outlook.fr>
2024-02-18 14:59:18 +0100

    dfsdf

--- srcs/quit.c
+++ srcs/quit.c
@@ -6,7 +6,7 @@
 /*   By: cesar <cesar@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/11 21:12:28 by cesar             #+#    #+#             */
-/*   Updated: 2023/12/13 08:16:07 by cesar            ###   ########.fr       */
+/*   Updated: 2024/02/17 16:20:22 by cesar            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,6 @@
 
 void	quit(char *quit_msg)
 {
-	write(STDERR_FILENO, "\nError: ", 9);
 	write(STDERR_FILENO, quit_msg, ft_strlen(quit_msg));
-	write(STDERR_FILENO, "\n", 1);
 	exit(EXIT_FAILURE);
 }
